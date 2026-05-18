f869cc55d1c7b8e5026a0322ff5eed6a48453d38
ucup233 <sufhidayat9@gmail.com>
2026-05-18 14:58:11 +0700

    Pembuatan model dto

--- src/main/java/com/example/week11/model/dto/RegisterRequest.java
+++ src/main/java/com/example/week11/model/dto/RegisterRequest.java
@@ -1,4 +1,12 @@
 package com.example.week11.model.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String username;
+    private String password;
+
+    private String nama;
+    private String alamat;
 }
