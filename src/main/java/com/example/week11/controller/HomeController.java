eb1432320998209449914f326f0bd4701f84ae91
ucup233 <sufhidayat9@gmail.com>
2026-05-18 15:05:29 +0700

    pembuatan home controller

--- src/main/java/com/example/week11/controller/HomeController.java
+++ src/main/java/com/example/week11/controller/HomeController.java
@@ -1,4 +1,26 @@
 package com.example.week11.controller;
 
+import com.example.week11.model.User;
+import com.example.week11.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final AuthService authService;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        User user = authService.getLoggedInUser();
+
+        model.addAttribute("user", user);
+        model.addAttribute("profile", user.getProfile());
+
+        return "home";
+    }
 }
