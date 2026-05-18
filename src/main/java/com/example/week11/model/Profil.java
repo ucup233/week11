cdf33a30189da8cc7bce126fb41c829c2337fa7d
ucup233 <sufhidayat9@gmail.com>
2026-05-18 14:59:02 +0700

    pembuatan model profiles

--- src/main/java/com/example/week11/model/Profil.java
+++ src/main/java/com/example/week11/model/Profil.java
@@ -1,4 +1,25 @@
 package com.example.week11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profil {
+    @Id
+    private String id;
+
+    private String nama;
+    private String alamat;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
