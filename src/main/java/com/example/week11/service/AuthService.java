a64bc409b19bd5c0a7661e0df3758eb6c73417d8
ucup233 <sufhidayat9@gmail.com>
2026-05-18 15:03:40 +0700

    pembuatan auth services

--- src/main/java/com/example/week11/service/AuthService.java
+++ src/main/java/com/example/week11/service/AuthService.java
@@ -1,4 +1,52 @@
 package com.example.week11.service;
 
+import com.example.week11.model.Profil;
+import com.example.week11.model.User;
+import com.example.week11.model.dto.RegisterRequest;
+import com.example.week11.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(RegisterRequest request) {
+
+        User user = User.builder()
+                .id(UUID.randomUUID().toString())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        Profil profile = Profil.builder()
+                .id(UUID.randomUUID().toString())
+                .nama(request.getNama())
+                .alamat(request.getAlamat())
+                .user(user)
+                .build();
+
+        user.setProfile(profile);
+
+        userRepository.save(user);
+    }
+
+    public User getLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username = authentication.getName();
+
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User tidak ditemukan"));
+    }
 }
