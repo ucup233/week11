05cdd5fb3deb037bc1a9a3f04e4bb24fe08c3135
ucup233 <sufhidayat9@gmail.com>
2026-05-18 14:59:24 +0700

    pembuatan model users

--- src/main/java/com/example/week11/model/User.java
+++ src/main/java/com/example/week11/model/User.java
@@ -1,4 +1,24 @@
 package com.example.week11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    private String id;
+
+    private String username;
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profil profile;
 }
